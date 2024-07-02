2fa5d8aeb5939ef2e3de6f4cb4fd9cfc3dcd55b7
aditya8850 <kalsotraaditya98@gmail.com>
2024-07-02 15:09:37 +0530

    "Refactored FriendController and FriendshipRepo: added semicolons, fixed indentation, and updated error handling"

--- src/features/friendship/friend.controller.js
+++ src/features/friendship/friend.controller.js
@@ -3,54 +3,55 @@ import { ApplicationError } from "../../error-handler/errorHandler.js";
 
 export default class FriendController {
     constructor() {
-        this.friendshipRepo = new friendshipRepo()
+        this.friendshipRepo = new friendshipRepo();
     }
 
     async toggleFriendship(req, res, next) {
         try {
-            const friendId = req.params.friendId
-            const userId = req.userId
+            const friendId = req.params.friendId;
+            const userId = req.userId;
+
             if (friendId == userId) {
-                throw new ApplicationError("You can't add yourself as a friend", 400)
+                return res.status(400).json({ message: "You can't add yourself as a friend" });
             }
-            const resp = await this.friendshipRepo.toggleFriendRepo(userId, friendId)
-            if (resp) {
-                res.status(200).json({ message: resp.message, data: resp.data })
-            }
+
+            const resp = await this.friendshipRepo.toggleFriendRepo(userId, friendId);
+            res.status(200).json({ message: resp.message, data: resp.data });
         } catch (error) {
-            throw new ApplicationError('Err while toggling friendreq.', 404)
+            next(new ApplicationError('Error while toggling friend request.', 500));
         }
     }
 
     async responseToFriendship(req, res, next) {
-        const friendId = req.params.friendId
-        const { status } = req.query
-        const userId = req.userId
-        const resp = await this.friendshipRepo.responseToFriendshipRepo(userId, friendId, status)
-        if (resp) {
-            res.status(200).json({ message: resp.message ,data:resp.data})
-        }else{
-            throw new ApplicationError("Err responding to friendreq.",404)
+        try {
+            const friendId = req.params.friendId;
+            const { status } = req.query;
+            const userId = req.userId;
+
+            const resp = await this.friendshipRepo.responseToFriendshipRepo(userId, friendId, status);
+            res.status(200).json({ message: resp.message, data: resp.data });
+        } catch (error) {
+            next(new ApplicationError("Error responding to friend request.", 500));
         }
     }
 
     async getPendingReq(req, res, next) {
         try {
-            const userId = req.userId
-            const resp = await this.friendshipRepo.getPendingRepo(userId)
-            res.status(200).json({ resp })
+            const userId = req.userId;
+            const pendingRequests = await this.friendshipRepo.getPendingRepo(userId);
+            res.status(200).json({ data: pendingRequests });
         } catch (error) {
-
+            next(new ApplicationError('Error fetching pending requests.', 500));
         }
     }
 
-    async getUserFriends(req,res,next){
+    async getUserFriends(req, res, next) {
         try {
-            const userId = req.params.userId
-            const resp = await this.friendshipRepo.getUserFriendsRepo(userId)
-            res.status(200).json({resp})
+            const userId = req.params.userId;
+            const friends = await this.friendshipRepo.getUserFriendsRepo(userId);
+            res.status(200).json({ data: friends });
         } catch (error) {
-            throw new ApplicationError("err finding friends",404)
+            next(new ApplicationError("Error finding friends.", 500));
         }
     }
 }
